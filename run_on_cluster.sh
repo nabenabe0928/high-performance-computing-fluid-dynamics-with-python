88b701765b7e21b9738a138ed0dcf97cdc3dae0e
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-06-13 06:06:25 +0200

    [fix] Fix an issue in arunning script

--- run_on_cluster.sh
+++ run_on_cluster.sh
@@ -1,21 +1,16 @@
+# Setup commands for BWUniCluster
 module load devel/python/3.9.2_gnu_10.2
 module load mpi/openmpi/4.1
-
 pip install --user --upgrade mpi4py numpy matplotlib tqdm scipy
 
-# 10
-# 12
-# 15
-# 20
-# 25
-# 30
 # Use only half of the CPUs for the correct performance measurement
 # Note: 40 CPUs are available for each node
 
-NTASKS=40
-NS=(1 4 9 16 25 36 100 144 225 400 625 900)
-for N in ${NS[@]}; do
-    NODES=$((${N}/20+1))
-    echo sbatch --nodes=$NODES --ntasks-per-node=$NTASKS run_server.sh -N $N
-    sbatch --nodes=$NODES --ntasks-per-node=$NTASKS run_server.sh -N $N
+N_THREADS_LIST=(1 4 9 16 25 36 100 144 225 400 625 900)
+for N_THREADS in ${N_THREADS_LIST[@]}; do
+    NODES=$((${N_THREADS}/20+1))
+    echo "Use ${N_THREADS} threads on ${NODES} nodes."
+    echo sbatch --nodes=$NODES --ntasks-per-node=40 run_server.sh -N $N_THREADS
+    sbatch --nodes=$NODES --ntasks-per-node=40 run_server.sh -N $N_THREADS
+    echo ""
 done
