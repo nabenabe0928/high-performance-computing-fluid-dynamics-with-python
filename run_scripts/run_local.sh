83da530d673d7eca4e940a09cf79ecd9dda8ea91
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-07-30 21:01:48 +0200

    [doc] Complete the final report (beta version)

--- run_scripts/run_local.sh
+++ run_scripts/run_local.sh
@@ -2,7 +2,7 @@ cmd="python -m run_experiment -E cf -T 2400 -S 50 -W 0.5 --omega 0.35"
 echo $cmd
 $cmd
 
-cmd="python -m run_experiment -E pf -T 5000 -S 50 -I 0.301 -O 0.300 --omega 0.70"
+cmd="python -m run_experiment -E pf -T 5500 -S 50 -I 0.301 -O 0.300 --omega 0.70"
 echo $cmd
 $cmd
 
