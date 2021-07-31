ea164a070b8aa9db99c94ad5f8e9be2da76e1e7e
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-07-31 22:54:13 +0200

    [fix] Fix -n errors in shells

--- run_scripts/run_reynolds10000.sh
+++ run_scripts/run_reynolds10000.sh
@@ -9,6 +9,6 @@ module load devel/python/3.9.2_gnu_10.2
 module load mpi/openmpi/4.1
 
 echo "Running on ${SLURM_JOB_NUM_NODES} nodes with ${SLURM_JOB_CPUS_PER_NODE} cores each."
-echo "Use ${N} threads."
+echo "Use 100 threads."
 
-mpirun -n $N python -m run_experiment -E sm -T 100000 -X 1500 -Y 1500 --visc 0.03 -W 0.2
+mpirun -n 100 python -m run_experiment -E sm -T 100000 -X 1500 -Y 1500 --visc 0.03 -W 0.2
--- run_scripts/submit_reynolds10000.sh
+++ run_scripts/submit_reynolds10000.sh
@@ -3,5 +3,5 @@ module load mpi/openmpi/4.1
 pip install --user --upgrade mpi4py numpy matplotlib tqdm scipy
 
 NODES=3
-echo sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_reynolds10000.sh -N 100
-sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_reynolds10000.sh -N 100
+echo sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_reynolds10000.sh
+sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_reynolds10000.sh
