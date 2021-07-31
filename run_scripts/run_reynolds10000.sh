4f76d01ae21ff3eeca37adb3484cad4bf1950d7d
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-07-31 22:51:25 +0200

    [fix] Fix name erros in shells

--- run_scripts/run_reynolds10000.sh
+++ run_scripts/run_reynolds10000.sh
@@ -11,4 +11,4 @@ module load mpi/openmpi/4.1
 echo "Running on ${SLURM_JOB_NUM_NODES} nodes with ${SLURM_JOB_CPUS_PER_NODE} cores each."
 echo "Use ${N} threads."
 
-mpirun -n $N python -m run_experiment -E sm -T 100000 -X 1500 -Y 1500 --visc 0.03 -W 0.2 --scaling True
+mpirun -n $N python -m run_experiment -E sm -T 100000 -X 1500 -Y 1500 --visc 0.03 -W 0.2
--- run_scripts/submit_scaling_test.sh
+++ run_scripts/submit_scaling_test.sh
@@ -12,8 +12,8 @@ for S in 100 1000; do
     for N_THREADS in ${N_THREADS_LIST[@]}; do
         NODES=$((${N_THREADS}/40+1))
         echo "Use ${N_THREADS} threads on ${NODES} nodes."
-        echo sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_server.sh -N $N_THREADS -S $S
-        sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_server.sh -N $N_THREADS -S $S
+        echo sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_scaling_test.sh -N $N_THREADS -S $S
+        sbatch --nodes=$NODES --ntasks-per-node=40 run_scripts/run_scaling_test.sh -N $N_THREADS -S $S
         echo ""
     done
 done
