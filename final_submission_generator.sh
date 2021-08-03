f01e71957d61b8ae07de344b25dd5fad3b5dc2cc
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-08-03 17:56:27 +0200

    [doc] Final update

--- final_submission_generator.sh
+++ final_submission_generator.sh
@@ -1,15 +1,19 @@
 rm final_submission.zip
 mkdir final_submission/
-rsync -av log/ final_submission/log/ --exclude "*.npy"
+mkdir final_submission/src/
+
+cp -r src/utils final_submission/src/
+cp -r src/simulation_attributes final_submission/src/
+cp -r src/experiments.py final_submission/src/
+
 cp -r media/ final_submission/
 cp -r run_scripts/ final_submission/
-cp -r src/ final_submission/
-cp -r submission/ final_submission/
-cp -r test/ final_submission/
 cp README.md final_submission/
 cp requirements.txt final_submission/
 cp run_experiment.py final_submission/
-rm -r final_submission/submission/out final_submission/submission/memo.tex
+
+cp -r submission/submission.pdf final_submission/
+cp -r submission/out/submission.pdf final_submission/
 
 echo "### Create ZIP ###"
 
